cf8db4f91654fefcf33e6737b4c3ee4b4abaa2fc
pedroacamargo <pedroaugennes@gmail.com>
2024-10-11 15:59:45 +0100

    setup 2

---
+++ 2/hello.py
@@ -0,0 +1,15 @@
+import csv
+import re
+
+# Read the CSV file
+csv_file = 'emd.csv'  # Update with your actual CSV file path
+
+
+with open(csv_file, mode='r', encoding="utf-8") as file:
+    csv_string = file.read()
+    
+
+        
+
+
+# 1) Calcular as Idades extremas dos registos no dataset
