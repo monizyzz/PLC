d008c6342114fd996bd1e14a0b0e80c2c1b00441
bernazyzz <zebernardomoniz@gmail.com>
2024-12-30 10:26:20 +0000

    update lexer

--- TP2/lexerPMScript.py
+++ TP2/lexerPMScript.py
@@ -1,10 +1,16 @@
 import ply.lex as lex
 import sys
 
+INT = 'INT'
+FLOAT = 'FLOAT'
+ARRAY = 'ARRAY'
+STR = 'STR'
+
 class LexerPMScript(object):
     def __init__(self, debug = 0, optimize = 0, reflags = 0):
         self.lexer = lex.lex(module=self, debug=debug, optimize=optimize, reflags=reflags)
         self.lexer.lineno = 1
+        self.var = {}
     
     tokens = [
         'INTVALUE',
@@ -13,6 +19,15 @@ class LexerPMScript(object):
         'INC',
         'DEC',
         'ID',
+        'VARINT',
+        'VARFLOAT',
+        'VARSTRING',
+        'VARARRAY',
+        'GEQUAL',
+        'LEQUAL',
+        'EQUAL',
+        'DIFF',
+        'LINE'
     ]
     
     RESERVED = {
@@ -22,8 +37,16 @@ class LexerPMScript(object):
         'const': 'CONST',
         'let': 'LET',
         'Array': 'ARRAY',
+        'if': 'IF',
+        'else': 'ELSE',
+        'for': 'FOR',
+        'while': 'WHILE',
+        'console.log': 'PRINT', # podiamos ter algo para PRINTLN, pois existe isso na VM, tipo console.logln ?
         'console.input': 'INPUT',
         ';': 'SEMICOLON',
+        'or': 'OR',
+        'and': 'AND',
+        'not': 'NOT',
     }
         
     tokens += list(RESERVED.values())
@@ -37,9 +60,34 @@ class LexerPMScript(object):
     t_INTVALUE = r'\d+'
     t_FLOATVALUE = r'\d+\.\d+'
     t_STRINGVALUE = r'\"(^\"|[^"])*\"'
-    
-    
-    literals = [':', '=', '[', ']', '<', '>', ',', '(', ')']
+    t_GEQUAL = r'>='
+    t_LEQUAL = r'<='
+    t_EQUAL = r'=='
+    t_DIFF = r'!='
+    t_AND = r'&&'
+    t_OR = r'\|\|'
+
+    literals = [
+        ':', 
+        '=', 
+        '[', 
+        ']', 
+        '<', 
+        '>', 
+        ',', 
+        '(', 
+        ')',
+        '<', 
+        '>', 
+        ',', 
+        '+', 
+        '-', 
+        '*', 
+        '/',
+        '%',
+        '{', 
+        '}'
+    ]
 
     def t_SEMICOLON(self, t):
         r';'
@@ -47,7 +95,7 @@ class LexerPMScript(object):
         return t
     
     def t_ARRAY(self, t):
-        r'Array(?=<[INT|FLOAT|STR]>)'
+        r'Array(?=<(INT|FLOAT|STR)>)'
         return t
     
     def t_CONST(self, t):
@@ -61,12 +109,25 @@ class LexerPMScript(object):
     def t_ID(self, t):
         r'[a-zA-Z_][a-zA-Z_0-9\.]*'
         t.type = self.RESERVED.get(t.value, 'ID')
+        v = self.var.get(t.value, None)
+        if v is not None:
+            type = v[1]
+            if type == INT:
+                t.type = 'VARINT'
+            elif type == FLOAT:
+                t.type = 'VARFLOAT'
+            elif type == ARRAY:
+                t.type = 'VARARRAY'
+            elif type == STR:
+                t.type = 'VARSTRING'
         return t
     
     def t_NEWLINE(self, t):
         r'\n+'
         t.lexer.lineno += len(t.value)
         return t
+    
+    t_LINE = r'\"(^\"|[^"])*\"'
     
     t_ignore = ' \t\n'
     
